7ceff95432a21267e4ea08bbda8903f27dcce21d
Samer-Al-iraqi <samer.mhana@gmail.com>
2016-04-01 03:17:48 +0800

    Update queue.php

--- queue.php
+++ queue.php
@@ -118,22 +118,7 @@ $queue->displayQueueData();
 echo "Left value now is: ", $queue->left(), "\n";
 
 echo "right value now is: ", $queue->right(), "\n";
-?>
-Output: 
 
-Queue is Empty 
-Adding to left 3 values: 5,3,7 
-Queue: <5,3,7>
-Adding to LEFT 10 
-Queue: <10,5,3,7>
-Adding to RIGHT 25 
-Queue: <10,5,3,7,25>
-Remove from Left 
-Queue: <5,3,7,25>
-Remove from Right 
-Queue: <5,3,7>
-Left value now is: 5
-right value now is: 7
 
 
 
